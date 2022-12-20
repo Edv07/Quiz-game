c9218dda0706b4a3dcdf97d23ed2c87578d8dca4
Edgar <edvmaster.com@gmail.com>
2022-12-19 23:46:31 -0600

    Quiz working halfy

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,118 @@
-const startButton = document.getElementById("start-Btn")
-const questionContainerElement = document.getElementById
-('question-container')
+// define some const that are interactive or reactive in the quiz
 
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-startButton.addEventListener("click", startGame)
+let shuffledQuestions, currentQuestionIndex
 
-
+// when startButton is click a function startGame will run
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+ // startgame only happens if the start or restart button is pressed clicked.
 function startGame() {
-    console.log("Test")
-    startButton.classList.add('hide')
-    questionContainerElement.classList.remove('hide')
-    setNextQuestion()
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+// selects new questions and resets to not show previous questions
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-
-function setNextQuestion () {
-
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+// 
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
 }
 
-function selectAnswer (){
-
-
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
 }
 
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+// list of questions
+const questions = [
+  {
+    question: 'Which is a data type in javascript?',
+    answers: [
+      { text: 'boolean' },
+      { text: 'conversion' },
+      { text: 'style' },
+      { text: 'Netscape' }
+    ]
+  },
+  {
+    question: 'What is === operator?',
+    answers: [
+      { text: 'strict equality operator' },
+      { text: 'not equal operator'},
+      { text: 'greater'  },
+      { text: 'less' }
+    ]
+  },
+  {
+    question: ' what do you type to start a new variable',
+    answers: [
+      { text: 'bar' },
+      { text: 'var' },
+      { text: 'zar' },
+      { text: 'abile'}
+    ]
+  },
+  {
+    question: 'Is JavaScript a case-sensitive language??',
+    answers: [
+      { text: 'yes' },
+      { text: 'no' }
+    ] 
+  }
+ 
+]
